bed01515cace2752e6aaba19c2ccd937b2f968b7
Rick Mealy <rickmealy@gmail.com>
2022-03-11 19:39:28 -0800

    Added grid axes and point labels.

--- main.lua
+++ main.lua
@@ -4,27 +4,25 @@
 -- TODO: Draw multiple lines
 
 --constants
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
 POINT_SIZE_INCREASE = 0
 
-X_CENTER = 400
-Y_CENTER = 300
-
---[[
-x0 = X_CENTER + 60
-y0 = Y_CENTER + 240
-
-x1 = X_CENTER - 50
-y1 = Y_CENTER - 200
-]]--
-
-x0 = X_CENTER + 0
-y0 = Y_CENTER + 0
-
-x1 = X_CENTER + 0
-y1 = Y_CENTER + 0
-
+-- P0 (start point)
+P0X = -100
+P0Y = 200
+-- P1 (end point)
+P1X = 250
+P1Y = -100
 
 function love.load()
+   -- copy constants to variables
+   x0 = P0X
+   y0 = P0Y
+   x1 = P1X
+   y1 = P1Y
+   
    -- load a table with x & y coordinates of all points
    points = {}
 
@@ -32,6 +30,7 @@ function love.load()
       -- line is horizontal-ish
       if x0 > x1 then
 	 x0, x1 = x1, x0
+	 y0, y1 = y1, y0
       end
       ys = interpolate(x0, y0, x1, y1)
       for x=x0,x1 do
@@ -41,6 +40,7 @@ function love.load()
    else
       -- line is vertical-ish
       if y0 > y1 then
+	 x0, x1 = x1, x0
 	 y0, y1 = y1, y0
       end
       xs = interpolate(y0, x0, y1, x1)
@@ -50,6 +50,19 @@ function love.load()
       end
    end
 
+   -- loop thru points table and convert from cartesian to love2d coordiantes
+   odd = true
+   for i,value in ipairs(points) do -- temp note: i starts at 1
+      -- print(i .. " " .. value)
+      if odd == true then
+	 points[i] = convert2screen(value, true)
+	 odd = false
+      else
+	 points[i] = convert2screen(value, false)
+	 odd = true
+      end
+   end
+   
    -- print("table size = " .. #points)
    
    -- print(love.graphics.getPointSize())
@@ -61,12 +74,21 @@ function love.load()
 end
 
 function love.draw()
---   love.graphics.print("Hello World", 400, 300)
+   -- love.graphics.print("Hello World", 400, 300)
    -- love.graphics.clear(0.0,0.0,0.0) -- set screen color to rgb values
 
    -- love.graphics.rectangle("line", x1, y0, x0 - x1, y1 - y0)
 
+   -- draw x and y axes for cartesian plane
+   love.graphics.line(SCREEN_WIDTH/2, 0, SCREEN_WIDTH/2, SCREEN_HEIGHT)
+   love.graphics.line(0, SCREEN_HEIGHT/2, SCREEN_WIDTH, SCREEN_HEIGHT/2)
+
+   love.graphics.print("P0", convert2screen(P0X, true), convert2screen(P0Y, false))
+   love.graphics.print("P1", convert2screen(P1X, true), convert2screen(P1Y, false))
+   
+   love.graphics.setColor(1,1,0)
    love.graphics.points(points)
+   love.graphics.setColor(1,1,1)
 end
 
 function love.keypressed(key, scancode, isrepeat)
@@ -88,5 +110,14 @@ function interpolate(i0, d0, i1, d1)
       d = d + a
    end
    return values
+end
+
+-- convert point coordinate from cartesian to love2d screen
+function convert2screen(value, isX)
+   if isX == true then
+      return value + SCREEN_WIDTH/2
+   else
+      return SCREEN_HEIGHT/2 - value
+   end
 end
 
