0e192e9420554d89eb0466f32fd395206faf3a22
Rick Mealy <rickmealy@gmail.com>
2022-03-14 17:47:35 -0700

    Completed filled triangle. Added my-love2d-notes text file.

--- filled-triangles/main.lua
+++ filled-triangles/main.lua
@@ -1,13 +1,11 @@
 -- draw a filled triangle
 -- default window size is 800x600
 
--- TODO: Major bug somewhere. Filled triangle has different rotation, scale, and/or shape as outline.
-
 --constants
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-POINT_SIZE_INCREASE = 0
+POINT_SIZE_INCREASE = 1
 -- triangle points
 -- P0
 P0X = 20
@@ -31,17 +29,22 @@ P2X = -200
 P2Y = -250
 ]]--
 
+DRAW_OUTLINE = true
+DRAW_FILL = true
+
 function love.load()
    -- copy constants to variables to preserve original point values
    x0, y0 = P0X, P0Y
    x1, y1 = P1X, P1Y
    x2, y2 = P2X, P2Y
-   
+
    -- get points for edge lines
    points01 = calculatePoints(x0, y0, x1, y1)
    points12 = calculatePoints(x1, y1, x2, y2)
    points20 = calculatePoints(x2, y2, x0, y0)
 
+   -- debug()
+   
    -- get fill points for each triangle edge
    -- sort points so y0 <= y1 <= y2
    if y1 < y0 then
@@ -56,11 +59,15 @@ function love.load()
       x2, x1 = x1, x2
       y2, y1 = y1, y2
    end
+
+   -- debug()
    
    -- get list of x coordinates for each edge
    x01 = interpolate(y0, x0, y1, x1)
    x12 = interpolate(y1, x1, y2, x2)
    x02 = interpolate(y0, x0, y2, x2)
+
+   -- debug()
    
    -- concatenate the short sides
    table.remove(x01)
@@ -83,7 +90,7 @@ function love.load()
       x_right = x02
    end
 
-   print("Size of x_left = " .. #x_left)
+   -- print("Size of x_left = " .. #x_left)
    
    -- prepare for drawing, but only done once
    ps = love.graphics.getPointSize() + POINT_SIZE_INCREASE
@@ -101,24 +108,29 @@ function love.draw()
    love.graphics.print("P2", convert2screen(P2X, true), convert2screen(P2Y, false))
    ]]--
 
+   if DRAW_OUTLINE == true then
    -- draw triangle outline
-   love.graphics.setColor(1,1,0)
-   love.graphics.points(points01)
-   love.graphics.points(points12)
-   love.graphics.points(points20)
-   love.graphics.setColor(0,0,0)
-
-   -- fill triangle
-   love.graphics.setColor(0,1,0)
-
-   for y=y0,y2 do
-      for x=x_left[y - y0 + 1],x_right[y - y0 + 1] do
-	--  print("x = " .. x .. ", y = " .. y)
-	 love.graphics.points(convert2screen(x, true), convert2screen(y, false))
-      end
+      love.graphics.setColor(1,1,0) -- yellow
+      love.graphics.points(points01)
+      love.graphics.points(points12)
+      love.graphics.points(points20)
+      love.graphics.setColor(1,1,1) -- white
    end
+   
+   if DRAW_FILL == true then
+      -- fill triangle
+      love.graphics.setColor(0,1,0)
 
-   love.graphics.setColor(0,0,0)   
+      for y=y0,y2 do
+	 for x=x_left[y - y0 + 1],x_right[y - y0 + 1] do
+	    --  print("x = " .. x .. ", y = " .. y)
+	    love.graphics.points(convert2screen(x, true), convert2screen(y, false))
+	 end
+      end
+
+      love.graphics.setColor(1,1,1)   
+   end
+   
 end
 
 function love.keypressed(key, scancode, isrepeat)
@@ -140,7 +152,7 @@ function calculatePoints(x0, y0, x1, y1)
       ys = interpolate(x0, y0, x1, y1)
       for x=x0,x1 do
 	 table.insert(points, x)
-	 table.insert(points, math.floor(table.remove(ys) + 0.5))
+	 table.insert(points, math.floor(table.remove(ys,1) + 0.5))
       end
    else
       -- line is vertical-ish
@@ -150,7 +162,7 @@ function calculatePoints(x0, y0, x1, y1)
       end
       xs = interpolate(y0, x0, y1, x1)
       for y=y0,y1 do
-	 table.insert(points, math.floor(table.remove(xs) + 0.5))
+	 table.insert(points, math.floor(table.remove(xs,1) + 0.5))
 	 table.insert(points, y)
       end
    end
@@ -179,7 +191,8 @@ function interpolate(i0, d0, i1, d1)
    a = (d1 - d0) / (i1 - i0)
    d = d0
    for i=i0,i1 do
-      table.insert(values, 1, d) -- insert at beginning
+      -- table.insert(values, 1, d) -- insert at beginning
+      table.insert(values, d) -- insert at end
       d = d + a
    end
    return values
@@ -194,3 +207,12 @@ function convert2screen(value, isX)
    end
 end
 
+function debug()
+   print("Point 0: " .. x0 .. " " .. y0)
+   print("Point 1: " .. x1 .. " " .. y1)
+   print("Point 2: " .. x2 .. " " .. y2)
+   print()
+   print("x01: " .. x01[1] .. " to " .. x01[#x01])
+   print("x12: " .. x12[1] .. " to " .. x12[#x12])
+   print("x02: " .. x02[1] .. " to " .. x02[#x02])
+end
